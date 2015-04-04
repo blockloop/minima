22050dd66b6f563817a418c076988708236f88ce
Brett Jones <bjones027@gmail.com>
2015-04-04 14:09:01 -0500

    lint fix

--- server/index.js
+++ server/index.js
@@ -1,28 +1,28 @@
 // BASE SETUP
 // =============================================================================
-var express = require("express");
-var bodyParser = require("body-parser");
+var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
-var morgan = require("morgan");
-var path = require("path");
-var mongoose = require("mongoose");
-var ArticleLoader = require("./article-loader");
-var articleLoader = new ArticleLoader();
-var logger = require("./logger");
-var sprintf = require("sprintf-js").sprintf;
+var morgan = require('morgan');
+var path = require('path');
+var mongoose = require('mongoose');
+var ArticleLoader = require('./article-loader');
+var logger = require('./logger');
+var sprintf = require('sprintf-js').sprintf;
 
+var articleLoader = new ArticleLoader();
 
 // VARS
-var STATIC_PATH = path.join(__dirname, "../static/");
-var HOSTNAME = process.env.HOSTNAME || "localhost";
+var STATIC_PATH = path.join(__dirname, '../static/');
+var HOSTNAME = process.env.HOSTNAME || 'localhost';
 var PORT = process.env.PORT || 4000;
-var isProd = process.env.NODE_ENV === "production";
+var isProd = process.env.NODE_ENV === 'production';
 
 // configure app
 app.use(articleLoader.loader);
 
 app.use(morgan({
-    format: isProd ? "tiny" : "dev",
+    format: isProd ? 'tiny' : 'dev',
     skip: function (req, res) {
         return isProd && res.statusCode < 400;
     },
@@ -34,43 +34,43 @@ app.use(morgan({
 // configure body parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.set("view engine", "jade");
+app.set('view engine', 'jade');
 
 // set locals
-app.locals.moment = require("moment");
-app.locals.config = require("../app.config");
+app.locals.moment = require('moment');
+app.locals.config = require('../app.config');
 
 // setup db
-mongoose.connect("mongodb://localhost:27017/minima");
-var router = require("./routes");
+mongoose.connect('mongodb://localhost:27017/minima');
+var router = require('./routes');
 app.use(express.static(STATIC_PATH, {
     maxAge: isProd ? 1000 * 60 * 60 : 0 // 60 mins
 }));
 
 
 // DEV
-if (process.env.NODE_ENV !== "production") {
-    require("express-livereload")(app, {
-        watchDir: "../views"
+if (process.env.NODE_ENV !== 'production') {
+    require('express-livereload')(app, {
+        watchDir: '../views'
     });
 }
 
 // REGISTER ROUTES
 // =============================================================================
-app.use("/", router);
+app.use('/', router);
 
 
 // LAST ERROR HANDLER
 // =============================================================================
 app.use(function(err, req, res, next) {
     if (err) {
-        var msg = sprintf("%s\n%s", err.message, err.stack);
+        var msg = sprintf('%s\n%s', err.message, err.stack);
         logger.error(msg);
         res.status(500);
 
         if (isProd) {
             // TODO create a pretty error page
-            res.end("500: An error occured");
+            res.end('500: An error occured');
         } else {
             res.end(msg);
         }
@@ -83,5 +83,5 @@ app.use(function(err, req, res, next) {
 // START THE SERVER
 // =============================================================================
 app.listen(PORT, HOSTNAME, function(){
-    logger.info("Listening at http://%s:%s", HOSTNAME, PORT);
+    logger.info('Listening at http://%s:%s', HOSTNAME, PORT);
 });
